ae0b89b463242dbcbb411de63f3d468de435f891
shan <sh.an@zipida.com>
2024-02-21 18:03:55 +0900

    Applying(page) : movie

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from "react";
-import {ActivityIndicator, Dimensions} from "react-native";
+import {ActivityIndicator, Dimensions, StyleSheet, useColorScheme} from "react-native";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import styled from "styled-components/native";
-import Swiper from "react-native-web-swiper";
+import Swiper from "react-native-swiper";
 import {makeImagePath} from "../utils";
 import {BlurView} from "expo-blur";
 
@@ -19,9 +19,39 @@ const Loading = styled.View`
     align-items: center;
 `;
 const BackgroundImage = styled.Image`
-    flex: 1;
+    width: 100%;
+    height: 100%;
+    position: absolute;
 `;
-const Title = styled.Text`
+const PosterImage = styled.Image`
+    width: 100px;
+    height: 160px;
+    border-radius: 5px;
+`;
+const Title = styled.Text<{ isDark: boolean }>`
+    font-size: 16px;
+    font-weight: 600;
+    color: ${(props) => props.isDark? "white" : "black"};
+`;
+const Overview = styled.Text<{ isDark: boolean }>`
+    margin-top: 5px;
+    color: ${(props) => props.isDark? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+/**
+ * styled(...) 라고 했을 시에 전체 css 를 가져옴
+ */
+const Votes = styled(Overview)`
+    margin-top: 10px;
+`;
+const Wrapper = styled.View`
+    flex-direction: row;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+const Column = styled.View`
+    width: 40%;
+    margin-left: 15px;
 `;
 
 /**
@@ -31,6 +61,7 @@ const Title = styled.Text`
 const {height: SCREEN_HEIGHT} = Dimensions.get("window");
 
 const Screen: React.FC<NativeStackScreenProps<any, "Movie">> = ({navigation: {navigate}}) => {
+    const isDark = useColorScheme() === 'dark';
     const [loading, setLoading] = useState(true);
     const [nowPlaying, setNowPlaying] = useState([]);
     const getNowPlaying = async () => {
@@ -59,21 +90,38 @@ const Screen: React.FC<NativeStackScreenProps<any, "Movie">> = ({navigation: {na
         </Loading>)
         : (<Container>
             <Swiper
+                horizontal
                 loop
-                timeout={3.5}
-                controlsEnabled={false}
+                autoplay
+                autoplayTimeout={3.5}
+                showsButtons={false}
+                showsPagination={false}
                 containerStyle={{
                     width: "100%",
                     height: SCREEN_HEIGHT / 4,
                 }}>
                 {
-                    nowPlaying.map(movie =>{
+                    nowPlaying.map(movie => {
                         return (<View key={movie['id']}>
                             <BackgroundImage source={{
                                 uri: makeImagePath(movie['backdrop_path'])
                             }}/>
-                            <BlurView>
-                                <Title>{movie['original_title']}</Title>
+                            <BlurView
+                                intensity={80}
+                                tint={isDark ? 'dark' : 'light'}
+                                style={StyleSheet.absoluteFill}>
+                                <Wrapper>
+                                    <PosterImage source={{uri: makeImagePath(movie['poster_path'])}}/>
+                                    <Column>
+                                        <Title isDark={isDark}>{movie['original_title']}</Title>
+                                        {
+                                            movie['vote_average'] > 0 ? (
+                                                <Votes isDark={isDark}>{`★${movie['vote_average']}/10`}</Votes>
+                                            ) : null
+                                        }
+                                        <Overview isDark={isDark}>{`${movie['overview'].slice(0, 100)}${movie['overview'].length > 100 ? "..." : ""}`}</Overview>
+                                    </Column>
+                                </Wrapper>
                             </BlurView>
                         </View>)
                     })
