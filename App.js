970431fee1251fc6f8b9350d2e91689c87880896
shan <sh.an@zipida.com>
2024-02-27 17:46:06 +0900

    Do(Practice) : animated interpolation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import {Animated, Pressable} from "react-native";
+import {Animated, Dimensions, Pressable} from "react-native";
 import {useRef, useState} from "react";
 
 const Container = styled.View`
@@ -14,6 +14,9 @@ const Box = styled.View`
     width: 100px;
     height: 100px;
 `;
+
+const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get("screen");
+
 export default function App() {
     // 1. Animated.Value 를 사용한다
     // 2. 직접 Animated.Value 값을 설정하지 않는다
@@ -23,34 +26,42 @@ export default function App() {
 
     // state 는 rerender 하기 때문에 animation 관련 상태값을 저장하기 위해서는 useRef 를 사용해야한다
     // useRef 를 사용하면 초기화된다고 해서 reset 되지 않음
-    const Y_POSITION = useRef(new Animated.Value(200)).current;
+    const POSITION = useRef(new Animated.ValueXY({x: 0, y: 200})).current;
     const [up, setUp] = useState(false);
     // 생성되어있는 Component 를 Animated 로 바꾸는 방법
     const AnimatedBox = Animated.createAnimatedComponent(Box);
 
     const toggleUp = () => {
         setUp(prev => !prev)
-        Y_POSITION.addListener(({value}) => {
-            // console.log(value)
-        })
     }
     const moveUp = () => {
-        Animated.timing(Y_POSITION, {
+        Animated.timing(POSITION, {
             toValue: up ? 200 : -200,
-            useNativeDriver: true,
+            // toValue: up ? {
+            //     x: 200,
+            //     y: 200
+            // } : {
+            //     x: -200,
+            //     y: -200
+            // },
+            useNativeDriver: false, // useNativeDriver: true 하니 addListener 없이 작동안함
         }).start(toggleUp);
     }
-    // 왜 addListener 없으면 작동이 정상으로안되지..?
-    Y_POSITION.addListener(({value}) => {
-        // console.log(value)
-    })
-    const opacityValue = Y_POSITION.interpolate({
+    const opacityValue = POSITION.y.interpolate({
         inputRange: [-200, 0, 200],
         outputRange: [1, 0, 1]
-    })
-    const borderRadius = Y_POSITION.interpolate({
+    });
+    const borderRadius = POSITION.y.interpolate({
         inputRange: [-200, 200],
         outputRange: [100, 0]
+    });
+    const rotation = POSITION.y.interpolate({
+        inputRange: [-200, 200],
+        outputRange: ["-360deg", "360deg"]
+    });
+    const bgColor = POSITION.y.interpolate({
+        inputRange: [-200, 200],
+        outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"]
     })
     return (
         <Container>
@@ -58,9 +69,12 @@ export default function App() {
                 <AnimatedBox style={{
                     opacity: opacityValue,
                     borderRadius: borderRadius,
-                    transform: [{
-                        translateY: Y_POSITION,
-                    }]
+                    backgroundColor: bgColor,
+                    transform: [
+                        // {translateY: POSITION.y},
+                        // {translateX: POSITION.x},
+                        ...POSITION.getTranslateTransform(),
+                        {rotateY: rotation}]
                 }}/>
             </Pressable>
         </Container>
